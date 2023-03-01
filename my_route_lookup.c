3dfc04d33c515c9b2e963015404257fbf5e803da
rodrigodelama <rodrigo.delama@icloud.com>
2023-03-01 16:22:10 +0100

    init

--- my_route_lookup.c
+++ my_route_lookup.c
@@ -1,9 +1,14 @@
 //Rodrigo De Lama - 100451775
 
+#include <stdio.h>
+
 int main(int argc, char * argv[]) //argv[1] routing file argv[2] test file
 {
     // my_route_lookup <FIB FILE>                   <INPUT FILE>
     //                 routing_table_simple.txt     prueba0.txt
+
+    printf("Hello World!")
+    return 0;
 }
 
 /*
