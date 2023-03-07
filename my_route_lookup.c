b89017a38e647d203ef4d0858e81092a781b3cac
rodrigodelama <rodrigo.delama@icloud.com>
2023-03-07 23:46:52 +0100

    changed indenting

--- my_route_lookup.c
+++ my_route_lookup.c
@@ -8,30 +8,30 @@
 int main (int argc, char *argv[]) //argv[1] routing file argv[2] test file
 {
 
-if (argc != 3)
-{
-	printf("\nThe program must have 2 input arguments\n");
-    printf("USE:\n");
-    printf("\t./my_route_lookup <routingTable> <inputFile>\n");
-    printf("WHERE:\n");
-    printf("\t<routingTable>\n");
-    printf("\t\tThe path for the file containing the routing table\n");
-	printf("\t<inputFile>\n");
-    printf("\t\tThe path for the input file\n\n");
-    return -1;
-}
+    if (argc != 3)
+    {
+        printf("\nThe program must have 2 input arguments\n");
+        printf("USE:\n");
+        printf("\t./my_route_lookup <routingTable> <inputFile>\n");
+        printf("WHERE:\n");
+        printf("\t<routingTable>\n");
+        printf("\t\tThe path for the file containing the routing table\n");
+        printf("\t<inputFile>\n");
+        printf("\t\tThe path for the input file\n\n");
+        return -1;
+    }
 
-//TODO: Initialize needed files and manage the errors (function printIoExplanationError will print the different errors on each case)
+    //TODO: Initialize needed files and manage the errors (function printIoExplanationError will print the different errors on each case)
 
-int init = initializeIO(argv[1], argv[2]);
-//TODO: function that allows us to use the routing table and a file containing the addresses to be processed and print an error if we can't find anything
+    int init = initializeIO(argv[1], argv[2]);
+    //TODO: function that allows us to use the routing table and a file containing the addresses to be processed and print an error if we can't find anything
 
-if(init != 0)
-{
-	printf("The following error occured: ");
-	printIOExplanationError(init);
-	return -1;
-}
+    if(init != 0)
+    {
+        printf("The following error occured: ");
+        printIOExplanationError(init);
+        return -1;
+    }
 
     printf("Hello World!\n");
     return 0;
